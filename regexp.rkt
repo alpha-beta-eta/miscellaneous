d652aad17dd584480212c0df2f9969f728a3b26f
alpha-beta-eta <95096031+alpha-beta-eta@users.noreply.github.com>
2023-09-05 12:44:52 +0800

    Add files via upload

--- regexp.rkt
+++ regexp.rkt
@@ -1,56 +1,70 @@
 #lang racket
 (require "match.rkt")
-;<lang> ::= 0
-;        |  1
-;        |  <symbol>
-;        |  (* <lang>)
-;        |  (U <lang> <lang>)
-;        |  (o <lang> <lang>)
-(define (* L)
-  (cond ((eqv? L 0) 0)
-        ((eqv? L 1) 1)
-        (else `(* ,L))))
-(define (U L1 L2)
-  (cond ((eqv? L1 0) L2)
-        ((eqv? L2 0) L1)
-        (else `(U ,L1 ,L2))))
-(define (o L1 L2)
-  (cond ((eqv? L1 0) 0)
-        ((eqv? L1 1) L2)
-        ((eqv? L2 0) 0)
-        ((eqv? L2 1) L1)
-        (else `(o ,L1 ,L2))))
-(define (delta? L)
-  (match L
-    (0 #f)
-    (1 #t)
-    (,c (guard (symbol? c)) #f)
-    ((* ,L) #t)
-    ((U ,L1 ,L2)
-     (or (delta? L1) (delta? L2)))
-    ((o ,L1 ,L2)
-     (and (delta? L1) (delta? L2)))))
-(define (empty? L)
-  (match L
-    (0 #t)
-    (1 #f)
-    (,c (guard (symbol? c)) #f)
-    ((* ,L) (empty? L))
-    ((U ,L1 ,L2) (and (empty? L1) (empty? L2)))
-    ((o ,L1 ,L2) (or (empty? L1) (empty? L2)))))
-(define (D c L)
-  (match L
-    (0 0)
-    (1 0)
-    (,a (guard (symbol? a)) (if (eq? c a) 1 0))
-    ((* ,L) (o (D c L) (* L)))
-    ((U ,L1 ,L2) (U (D c L1) (D c L2)))
-    ((o ,L1 ,L2)
-     (if (delta? L1)
-         (U (o (D c L1) L2) (D c L2))
-         (o (D c L1) L2)))))
-(define (matches? w L)
-  (cond ((null? w) (delta? L))
-        ((empty? L) #f)
-        (else
-         (matches? (cdr w) (D (car w) L)))))
+;<exp> ::= zero
+;       |  one
+;       |  <char>
+;       |  (+ <exp> <exp>) : union
+;       |  (* <exp> <exp>) : concatenation
+;       |  (k <exp>) : kleene star
+(define (+ e1 e2)
+  (cond
+    ((eq? e1 'zero) e2)
+    ((eq? e2 'zero) e1)
+    (else `(+ ,e1 ,e2))))
+(define (* e1 e2)
+  (cond
+    ((eq? e1 'zero) 'zero)
+    ((eq? e2 'zero) 'zero)
+    ((eq? e1 'one) e2)
+    ((eq? e2 'one) e1)
+    (else `(* ,e1 ,e2))))
+(define (k exp)
+  (cond
+    ((eq? exp 'zero) 'one)
+    ((eq? exp 'one) 'one)
+    (else `(k ,exp))))
+(define (delta? exp)
+  (match exp
+    (zero #f)
+    (one #t)
+    (,c (guard (char? c)) #f)
+    ((+ ,e1 ,e2)
+     (or (delta? e1) (delta? e2)))
+    ((* ,e1 ,e2)
+     (and (delta? e1) (delta? e2)))
+    ((k ,e) #t)))
+(define (empty? exp)
+  (match exp
+    (zero #t)
+    (one #f)
+    (,c (guard (char? c)) #f)
+    ((+ ,e1 ,e2)
+     (and (empty? e1) (empty? e2)))
+    ((* ,e1 ,e2)
+     (or (empty? e1) (empty? e2)))
+    ((k ,e) #f)))
+(define (D exp c)
+  (match exp
+    (zero 'zero)
+    (one 'zero)
+    (,c^
+     (guard (char? c^))
+     (if (char=? c^ c) 'one 'zero))
+    ((+ ,e1 ,e2) (+ (D e1 c) (D e2 c)))
+    ((* ,e1 ,e2)
+     (if (delta? e1)
+         (+ (* (D e1 c) e2) (D e2 c))
+         (* (D e1 c) e2)))
+    ((k ,e) (* (D e c) (k e)))))
+(define (in? str exp)
+  (define l (string-length str))
+  (let iter ((i 0) (exp exp))
+    (cond
+      ((= i l) (delta? exp))
+      ((empty? exp) #f)
+      (else
+       (iter (add1 i)
+             (D exp (string-ref str i)))))))
+;(define L
+;  (* #\c (* (+ #\a #\d) (* (k (+ #\a #\d)) #\r))))
+;(in? "cadar" L)
